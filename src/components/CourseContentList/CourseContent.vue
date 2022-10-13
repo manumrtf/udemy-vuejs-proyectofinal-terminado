cb51adbeac2c1b7780cadd1f5323928a8544cbc9
ManuelMartinDev <manuelmartindev@outlook.com>
2022-10-13 11:50:21 +0200

    starting point

---
+++ src/components/CourseContentList/CourseContent.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex gap-2 p-4">
+    <span>1</span>
+    <li class="font-bold capitalize">
+      Create a new Nextjs and Typescript project
+    </li>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,20 @@
-import CourseShowCaseView from "../views/CourseShowCaseView.vue";
+import TopicShowCaseView from "../views/TopicShowCaseView.vue";
 import CourseView from "../views/CourseView.vue";
 import HomeView from "../views/HomeView.vue";
 import ClassRoomView from "../views/ClassRoomView.vue";
 import TopicListView from "../views/TopicListView.vue";
+import AuthView from "../views/AuthView.vue";
 import { createRouter, createWebHistory } from "vue-router";
-// 1. Define route components.
-// These can be imported from other files
-const About = { template: "<div>About</div>" };
 
-// 2. Define some routes
-// Each route should map to a component.
-// We'll talk about nested routes later.
 const routes = [
   { path: "/", component: HomeView },
-  { path: "/about", component: About },
   {
-    path: "/course",
-    name: "Course",
-    component: CourseShowCaseView,
+    path: "/topic",
+    name: "Topic",
+    component: TopicShowCaseView,
   },
   {
-    path: "/course-view",
+    path: "/course",
     name: "CourseView",
     component: CourseView,
   },
@@ -34,15 +28,16 @@ const routes = [
     name: "Topics",
     component: TopicListView,
   },
+  {
+    path: "/auth",
+    name: "Auth",
+    component: AuthView,
+  },
 ];
 
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
 const router = createRouter({
-  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
   history: createWebHistory(),
-  routes, // short for `routes: routes`
+  routes,
 });
 
 export default router;
